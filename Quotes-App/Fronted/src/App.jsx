0ceefed493fbd29accbc136263541dc84b3d162e
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-08-16 23:59:39 +0530

    initial commit

---
+++ Quotes-App/Fronted/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import MainNavigation from './pages/mainNavigation/MainNavigation'
+
+function App() {
+  return (
+<Fragment>
+  <MainNavigation/>
+</Fragment>
+  )
+}
+
+export default App

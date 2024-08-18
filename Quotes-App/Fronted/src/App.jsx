ec37a422706315ebd080145b3e6333366b412259
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-08-19 00:17:36 +0530

    initial commit

--- Quotes-App/Fronted/src/App.jsx
+++ Quotes-App/Fronted/src/App.jsx
@@ -1,11 +1,21 @@
 import React, { Fragment } from 'react'
-import MainNavigation from './pages/mainNavigation/MainNavigation'
+import MainNavigation from './components/mainNavigation/MainNavigation'
+import { Route, Routes } from 'react-router-dom'
 
+import AllQuotes from './components/pages/AllQuotes'
+
+import NewQoute from './components/pages/NewQuotes'
+
+import { } from 'react-router-dom'
 function App() {
   return (
-<Fragment>
-  <MainNavigation/>
-</Fragment>
+    <Fragment>
+      <MainNavigation />
+<Routes>
+  <Route path='/' element='{AllQuotes}'/>
+  <Route path='/New' element='{NewQuotes}' /> 
+</Routes>
+    </Fragment>
   )
 }
 
